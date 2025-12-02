b568968d479ecabf49a6755f71cdec5d09513821
parallels <parallels@debian-gnu-linux-12-6.localdomain>
2025-12-02 19:14:01 +0700

    add func build_inserttuple in bitmapinsert

--- contrib/bitmap_index/src/bitmapinsert.c
+++ contrib/bitmap_index/src/bitmapinsert.c
@@ -46,6 +46,13 @@ typedef struct BMTIDLOVBuffer
     BMTIDBuffer *bufs[BM_MAX_LOVITEMS_PER_PAGE];
 } BMTIDLOVBuffer;
 
+
+static void build_inserttuple(Relation rel, uint64 tidnum,
+							   ItemPointerData ht_ctid, TupleDesc tupDesc, 
+							   Datum *attdata, bool *nulls, BMBuildState *state);
+
+
+
 /*
  * _bitmap_buildinsert() -- insert an index tuple during index creation.
  */
@@ -134,3 +141,156 @@ _bitmap_doinsert(Relation rel, ItemPointerData ht_ctid, Datum *attdata,
     ReleaseBuffer(metabuf);
     pfree(scanKeys);
 }
+
+
+
+/*
+ * build_inserttuple() -- insert a new tuple into the bitmap index
+ *	during the bitmap index construction.
+ *
+ * Each new tuple has an assigned number -- tidnum, called a
+ * tid location, which represents the bit location for this tuple in
+ * a bitmap vector. To speed up the construction, this function does not
+ * write this tid location into its bitmap vector immediately. We maintain
+ * a buffer -- BMTidBuildBuf to keep an array of tid locations
+ * for each distinct attribute value.
+ *
+ * If this insertion causes the buffer to overflow, we write tid locations
+ * for enough distinct values to disk to accommodate this new tuple.
+ */
+static void
+build_inserttuple(Relation rel, uint64 tidnum,
+				  ItemPointerData ht_ctid  pg_attribute_unused(), TupleDesc tupDesc,
+				  Datum *attdata, bool *nulls, BMBuildState *state)
+{
+	Buffer 			metabuf;
+	BlockNumber		lovBlock;
+	OffsetNumber	lovOffset;
+	bool			blockNull;
+	bool			offsetNull;
+	BMTidBuildBuf *tidLocsBuffer;
+	int				attno;
+	bool			allNulls = true;
+	BMBuildHashKey  *entry;
+
+	CHECK_FOR_INTERRUPTS();
+
+	tidLocsBuffer = state->bm_tidLocsBuffer;
+
+	/* Check if all attributes have value of NULL. */
+	for (attno = 0; attno < tupDesc->natts; attno++)
+	{
+		if (!nulls[attno])
+		{
+			allNulls = false;
+			break;
+		}
+	}
+
+	metabuf = _bitmap_getbuf(rel, BM_METAPAGE, BM_WRITE);
+	
+	/*
+	 * if the inserting tuple has the value of NULL, then
+	 * the corresponding tid array is the first.
+	 */
+	if (allNulls)
+	{
+		lovBlock = BM_LOV_STARTPAGE;
+		lovOffset = 1;
+	}
+	else
+	{
+		bool found;
+		BMBuildLovData *lov;
+
+		if (state->lovitem_hash)
+		{
+		    BMBuildHashKey toLookup;
+		    toLookup.attributeValueArr = attdata;
+		    toLookup.isNullArr = nulls;
+
+			/* look up the hash to see if we can find the lov data that way */
+			entry = (BMBuildHashKey *)hash_search(state->lovitem_hash,
+												  (void*)&toLookup,
+												  HASH_ENTER, &found);
+			if (!found)
+			{
+				/* Copy the key values in case someone modifies them */
+				for(attno = 0; attno < tupDesc->natts; attno++)
+				{
+					Form_pg_attribute at = TupleDescAttr(tupDesc, attno);
+
+					if (entry->isNullArr[attno])
+						entry->attributeValueArr[attno] = 0;
+					else
+						entry->attributeValueArr[attno] = datumCopy(entry->attributeValueArr[attno], at->attbyval,
+																	at->attlen);
+				}
+
+				/*
+				 * If the inserting tuple has a new value, then we create a new
+				 * LOV item.
+				 */
+				create_lovitem(rel, metabuf, tidnum, tupDesc, attdata, 
+							   nulls, state->bm_lov_heap, state->bm_lov_index,
+							   &lovBlock, &lovOffset, state->use_wal);
+
+				lov = (BMBuildLovData *) (((char*)entry) + state->lovitem_hashKeySize );
+				lov->lov_block = lovBlock;
+				lov->lov_off = lovOffset;
+			}
+
+			else
+			{
+				lov = (BMBuildLovData *) (((char*)entry) + state->lovitem_hashKeySize );
+				lovBlock = lov->lov_block;
+				lovOffset = lov->lov_off;
+			}
+		}
+
+		else {
+			/*
+			 * Search the btree to find the right bitmap vector to append
+			 * this bit. Here, we reset the scan key and call index_rescan.
+			 */
+			for (attno = 0; attno<tupDesc->natts; attno++)
+			{
+				ScanKey theScanKey = (ScanKey)(((char*)state->bm_lov_scanKeys) +
+											   attno * sizeof(ScanKeyData));
+				if (nulls[attno])
+				{
+					theScanKey->sk_flags = SK_ISNULL;
+					theScanKey->sk_argument = attdata[attno];
+				}
+				else
+				{
+					theScanKey->sk_flags = 0;
+					theScanKey->sk_argument = attdata[attno];
+				}
+			}
+
+			index_rescan(state->bm_lov_scanDesc, state->bm_lov_scanKeys, tupDesc->natts, NULL, 0);
+
+			found = _bitmap_findvalue(state->bm_lov_heap, state->bm_lov_index,
+									  state->bm_lov_scanKeys, state->bm_lov_scanDesc,
+									  &lovBlock, &blockNull, &lovOffset, &offsetNull);
+
+			if (!found)
+			{
+				/*
+				 * If the inserting tuple has a new value, then we create a new
+				 * LOV item.
+				 */
+				create_lovitem(rel, metabuf, tidnum, tupDesc, attdata, 
+							   nulls, state->bm_lov_heap, state->bm_lov_index,
+							   &lovBlock, &lovOffset, state->use_wal);
+			}
+		}
+	}
+
+	buf_add_tid(rel, tidLocsBuffer, tidnum, state, lovBlock, lovOffset);
+	_bitmap_wrtbuf(metabuf);
+
+	CHECK_FOR_INTERRUPTS();
+}
+
